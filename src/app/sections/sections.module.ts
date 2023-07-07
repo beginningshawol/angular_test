95332a5563d7cab5a0b573e8ecc52712bddadfa7
beginningshawol <31559545+beginningshawol@users.noreply.github.com>
2023-07-07 17:21:05 +0900

    Added buttons for header

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MatDividerModule } from '@angular/material/divider'
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatMenuModule } from '@angular/material/menu'
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { MatDividerModule } from '@angular/material/divider'
   ],
   imports: [
     CommonModule,
-    MatDividerModule
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule
   ],
   exports: [
     HeaderComponent,
